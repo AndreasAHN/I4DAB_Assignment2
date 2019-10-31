5d49168613aefcda33d54fa1774fa38f54306931
martinsekd <201710685@post.au.dk>
2019-10-31 11:40:19 +0100

    edit context class

--- Resturant/ConnectSQL.cs
+++ Resturant/ConnectSQL.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace Resturant
 {
-    class ConnectSQL
+    class ConnectSQL : DbContext
     {
-        ConnectSQL()
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connectionString = "server=84.238.88.212;uid=I4DAB_HandIn2;pwd=I4DaB1234;database=I4DAB_HandIn2";
+            optionsBuilder.UseSqlServer("server=84.238.88.212;uid=I4DAB_HandIn2;pwd=I4DaB1234;database=I4DAB_HandIn2");
         }
     }
 }
