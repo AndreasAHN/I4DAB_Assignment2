7ca989a1662d1e3ed792cc568cefddda692afdf8
Andreas Hervert Nielsen <andreas.hervert@gmail.com>
2019-10-31 11:28:33 +0100

    opsætning

---
+++ ConnectSQL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resturant
+{
+    class ConnectSQL
+    {
+        ConnectSQL()
+        {
+            string connectionString = "server=84.238.88.212;uid=I4DAB_HandIn2;pwd=I4dab1234;database=I4DAB_HandIn2";
+        }
+    }
+}
