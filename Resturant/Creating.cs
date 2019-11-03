ef5cc61eb9a5a67bbf8385f301caa9280af2e5d3
martinsekd <201710685@post.au.dk>
2019-11-03 18:05:45 +0100

    added creating class

---
+++ Resturant/Creating.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resturant
+{
+    class Creating
+    {
+        public void addRestaurant(string address, string name, string type)
+        {
+            using (var db = new I4DAB_HandIn2Context())
+            {
+                var restaurant = new Resturant.Restaurant();
+                restaurant.Address = address;
+                restaurant.Name = name;
+                restaurant.Type = type;
+                db.SaveChanges();
+            }
+        }
+
+        public void addReview(int star, string text)
+        {
+            using (var db = new I4DAB_HandIn2Context())
+            {
+                var review = new Review();
+
+            }
+        }
+    }
+}
