5d9e4774dcdd35d83bb5dea13ad5817658e6570e
Andreas Hervert Nielsen <andreas.hervert@gmail.com>
2019-11-05 15:13:37 +0100

    ReadSql add

--- Resturant/ReadSQL.cs
+++ Resturant/ReadSQL.cs
@@ -6,5 +6,44 @@ namespace Resturant
 {
     class ReadSQL
     {
+        public ReadSQL()
+        {
+
+        }
+
+        public void ReadResturant()
+        {
+
+        }
+
+        public void ReadReview()
+        {
+
+        }
+
+        public void ReadDish()
+        {
+
+        }
+
+        public void ReadGuest()
+        {
+
+        }
+
+        public void ReadPerson()
+        {
+
+        }
+
+        public void ReadWaiter()
+        {
+
+        }
+
+        public void ReadTable()
+        {
+
+        }
     }
 }
