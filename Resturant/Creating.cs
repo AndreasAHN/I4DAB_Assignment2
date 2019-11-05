a52193557fd59e311004ee478292bc891642124c
martinsekd <201710685@post.au.dk>
2019-11-05 13:53:55 +0100

    tilfoj

--- Resturant/Creating.cs
+++ Resturant/Creating.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 
@@ -83,6 +84,11 @@ namespace Resturant
             }
 
 
+        }
+
+        public void addTable(int number, Restaurant restaurant, Waiter waiter)
+        {
+            
         }
 
 
