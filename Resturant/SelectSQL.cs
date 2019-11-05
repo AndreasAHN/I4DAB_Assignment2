1df30e6e58dc8715b71e708162eccdd1807361ce
TheEpicZelda <mub1997@hotmail.com>
2019-11-05 16:36:26 +0100

    SelectSQL ReviewAverage og RestaurantMenu

--- Resturant/SelectSQL.cs
+++ Resturant/SelectSQL.cs
@@ -1,23 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Resturant
 {
     class SelectSQL
     {
-        public void SelectAllRestaurant()
-        {
-            using (var db = new I4DAB_HandIn2Context())
-            {
-                foreach (var rest in db.Restaurant)
-                {
-                    Console.WriteLine(rest.Address);
-                }
-            }
-        }
-
-        public void SelectTypeRestaurants()
+        public void SelectTypeRestaurants(string type)
         {
             using (var db = new I4DAB_HandIn2Context())
             {
@@ -33,21 +23,36 @@ namespace Resturant
                 {
                     if(rest.Address == address)
                     {
+                        Console.WriteLine("Restaurant stars:{0:0.00}", ReviewAverage(address));
+                        Console.WriteLine("Menu:");
                         foreach(var dish in rest.RestaurantDish)
                         {
                             Console.WriteLine("Type:{0}",dish.Dish.Type);
                             Console.WriteLine("Price:{0:0.00}",dish.Dish.Price);
-
                         }
                     }
                 }
             }
         }
 
-        double ReviewAverage()
+        double ReviewAverage(string address)
         {
             double average = 0;
+            int? totalStars = 0;
+            int starAmount = 0;
 
+            using (var db = new I4DAB_HandIn2Context())
+            {
+                var reviewliste = db.Review.Where(r => r.AddresseNavigation.Address == address);
+                foreach(var review in reviewliste)
+                {
+                   
+                    totalStars += review.Stars;
+                    starAmount++;
+                }
+            }
+
+            average = (double)totalStars / starAmount;
 
             return average;
         }
