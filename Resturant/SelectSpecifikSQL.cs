da6e1e42a489cd3ba7a225ea19468519fc951a64
Andreas Hervert Nielsen <andreas.hervert@gmail.com>
2019-11-06 00:33:46 +0100

    rettelse af merge

--- Resturant/SelectSpecifikSQL.cs
+++ Resturant/SelectSpecifikSQL.cs
@@ -16,7 +16,7 @@ namespace Resturant
 
         public List<Review> selectReview(Restaurant resturant)
         {
-            List<Review> dataReviews = selectAllSQL.SelectAllReview();
+            //List<Review> dataReviews = selectAllSQL.SelectAllReview();
             List<Review> dataOutPut;// = new List<Review>();
             //for (int i = 0; i < dataReviews.Count; i++)
             //{
@@ -40,39 +40,69 @@ namespace Resturant
             return dataOutPut;
         }
 
-        
+
         public List<TableIns> selectTableIns(Restaurant resturant)
         {
-            List<TableIns> dataTableIns = selectAllSQL.SelectAllTable();
-            List<TableIns> dataOutPut = new List<TableIns>();
-            for (int i = 0; i < dataTableIns.Count; i++)
+            List<TableIns> dataOutPut;
+
+            using (var db = new I4DAB_HandIn2Context())
             {
-                if (dataTableIns[i].Addresse == resturant.Address)
-                {
-                    dataOutPut.Add(dataTableIns[i]);
-                    Console.WriteLine(dataTableIns[i].ToString());
-                }
+                dataOutPut = db.TableIns.Where(reviewOut => reviewOut.Addresse.StartsWith(resturant.Address)).ToList();
             }
 
-
+            for (int i = 0; i < dataOutPut.Count; i++)
+            {
+                Console.WriteLine(dataOutPut[i].ToString());
+            }
 
             return dataOutPut;
         }
 
 
-        public List<Person> selectPerson(Restaurant resturant)
+        public Person selectPerson(int id)
         {
-            List<Person> dataPerson = selectAllSQL.SelectAllPerson();
             List<Person> dataOutPut = new List<Person>();
-            for (int i = 0; i < dataPerson.Count; i++)
+
+            using (var db = new I4DAB_HandIn2Context())
             {
-                //dataPerson[i].Address == resturant.Address
-                if (true)
-                {
-                    dataOutPut.Add(dataPerson[i]);
-                    Console.WriteLine(dataPerson[i].ToString());
-                }
+                //dataOutPut = from item in db.Person where item.PersonId == id select item.PersonId;
+                dataOutPut = db.Person.Where(personOut => personOut.PersonId == id).ToList(); //.Select(personOut => personOut.PersonId)
             }
+
+            Console.WriteLine(dataOutPut[0].ToString());
+
+            return dataOutPut[0];
+        }
+
+
+        public List<Waiter> selectWaiter(Person person)
+        {
+            List<Waiter> dataOutPut = new List<Waiter>();
+
+            using (var db = new I4DAB_HandIn2Context())
+            {
+                dataOutPut = db.Waiter.Where(waiterOut => waiterOut.PersonId == person.PersonId).ToList();//.Select(personOut => personOut.PersonId).ToList();
+            }
+
+            for (int i = 0; i < dataOutPut.Count; i++)
+            {
+                Console.WriteLine(dataOutPut[i].ToString());
+            }
+
+            return dataOutPut;
+        }
+
+
+        public List<WaiterTableIns> selectWaiterTableIns(Waiter waiter, TableIns tableIns)
+        {
+            List<WaiterTableIns> dataOutPut = new List<WaiterTableIns>();
+
+            using (var db = new I4DAB_HandIn2Context())
+            {
+                dataOutPut = db.WaiterTableIns.Where(waiterTableInsOut => ((waiterTableInsOut.WaiterId == waiter.WaiterId) && (waiterTableInsOut.TableId == tableIns.TableId))).ToList();
+            }
+
+            Console.WriteLine(dataOutPut.ToString());
 
             return dataOutPut;
         }
