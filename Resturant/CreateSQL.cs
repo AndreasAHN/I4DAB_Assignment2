ad1a68fef6d3c84ba56235a15f902d8222b8ea41
martinsekd <201710685@post.au.dk>
2019-11-06 08:38:05 +0100

    edit in create

--- Resturant/CreateSQL.cs
+++ Resturant/CreateSQL.cs
@@ -16,117 +16,106 @@ namespace Resturant
 
         }
 
+        private I4DAB_HandIn2Context db = new I4DAB_HandIn2Context();
+
         public void addRestaurant(ref Restaurant restaurant)
         {
-            using (var db = new I4DAB_HandIn2Context())
-            {
+            
                 string addresse = restaurant.Address;
                 if (db.Restaurant.Any(r => r.Address == addresse))
                     return;
                 db.Restaurant.Add(restaurant);
                 db.SaveChanges();
-            }
+            
         }
 
         public void addReview(ref Review review, ref Restaurant restaurant)
         {
-            using (var db = new I4DAB_HandIn2Context())
-            {
-                review.Addresse = restaurant.Address;
+            review.Addresse = restaurant.Address;
 
                 db.Review.Add(review);
                 db.SaveChanges();
 
-            }
         }
 
         public void addDish(ref Dish dish, ref Review review, ref Restaurant restaurant, ref Guest guest)
         {
-            using (var db = new I4DAB_HandIn2Context())
-            {
 
-                dish.ReviewId = review.ReviewId;
+            dish.ReviewId = review.ReviewId;
+                db.Dish.Add(dish);
+                db.SaveChanges();
 
+                //var guest2 = db.Guest.Take(1).First();
                 var gd = new GuestDish();
                 gd.Dish = dish;
                 gd.Guest = guest;
 
                 dish.GuestDish.Add(gd);
-                db.Dish.Add(dish);
+                
 
                 db.SaveChanges();
+
+
             }
 
-
-        }
-
-        public void addGuest(ref Guest guest, ref Person person, ref TableIns table, ref Review review, ref Dish dish)
+        public void addGuest(ref Guest guest, ref Person person, ref TableIns table, ref Review review)
         {
-            using (var db = new I4DAB_HandIn2Context())
-            {
-                
-                guest.ReviewId = review.ReviewId;
+
+            guest.ReviewId = review.ReviewId;
                 guest.TableId = table.TableId;
                 guest.FkPersonId = person.PersonId;
                 db.Guest.Add(guest);
 
-                dish.ReviewId = review.ReviewId;
+                //dish.ReviewId = review.ReviewId;
 
-                var gd = new GuestDish();
-                gd.Dish = dish;
-                gd.Guest = guest;
+                //var gd = new GuestDish();
+                //gd.Dish = dish;
+                //gd.Guest = guest;
 
-                guest.GuestDish.Add(gd);
+                //guest.GuestDish.Add(gd);
 
                
                 db.SaveChanges();
-            }
 
 
         }
 
         public void addTable(ref TableIns table, ref Restaurant restaurant, ref Waiter waiter)
         {
-            using (var db = new I4DAB_HandIn2Context())
-            {
+            
                 table.Addresse = restaurant.Address;
+                db.TableIns.Add(table);
+                db.SaveChanges();
 
                 var waiterTable = new WaiterTableIns();
 
                 waiterTable.TableId = table.TableId;
                 waiterTable.WaiterId = waiter.WaiterId;
-                db.TableIns.Add(table);
+                
                 db.WaiterTableIns.Add(waiterTable);
                 db.SaveChanges();
-               
-            }
+                
         }
 
-        public void addWaiter(ref Waiter waiter, Person person, TableIns table)
+        public void addWaiter(ref Waiter waiter, ref Person person)
         {
-            using (var db = new I4DAB_HandIn2Context())
-            {
-                waiter.PersonId = person.PersonId;
+            waiter.PersonId = person.PersonId;
 
-                var waiterTable = new WaiterTableIns();
-                waiterTable.TableId = table.TableId;
-                waiterTable.WaiterId = waiter.WaiterId;
+                //var waiterTable = new WaiterTableIns();
+                //waiterTable.TableId = table.TableId;
+                //waiterTable.WaiterId = waiter.WaiterId;
 
                 db.Waiter.Add(waiter);
-                db.WaiterTableIns.Add(waiterTable);
+                //db.WaiterTableIns.Add(waiterTable);
                 db.SaveChanges();
 
-            }
         }
 
         public void addPerson(ref Person person)
         {
-            using (var db = new I4DAB_HandIn2Context())
-            {
-                db.Person.Add(person);
+            db.Person.Add(person);
                 db.SaveChanges();
 
-            }
         }
 
 
--- Resturant/Program.cs
+++ Resturant/Program.cs
@@ -12,8 +12,12 @@ namespace Resturant
             var select = new SelectSQL();
 
             var person = new Person(){Name = "Henrik"};
-            var
-            var rest = new Restaurant(){Address = "Finlandsgade 2",Name = "Flammen",Type = "Buffet"};
+            var waiter = new Waiter() {Salary = 150};
+            var table = new TableIns(){Number = 5};
+            var guest = new Guest() {Reservation = DateTime.Now};
+            var dish = new Dish() {Price = 40, Type = "Dessert"};
+
+            var rest = new Restaurant(){Address = "Randersvej 6",Name = "Restaurant2",Type = "Buffet"};
             var review = new Review() {Text = "Det var ok", Stars = 3};
 
             create.addRestaurant(ref rest);
@@ -22,6 +26,14 @@ namespace Resturant
             create.addReview(ref review, ref rest);
 
             select.getAllWithType("Buffet");
+
+            create.addPerson(ref person);
+            create.addWaiter(ref waiter,ref person);
+            create.addTable(ref table,ref rest,ref waiter);
+            create.addGuest(ref guest,ref person,ref table,ref review);
+            create.addDish(ref dish, ref review, ref rest,ref guest);
+
+
             Console.ReadLine();
             //SelectAllSQL selectAllSQL = new SelectAllSQL();
             //var data = selectAllSQL.SelectAllResturant();
